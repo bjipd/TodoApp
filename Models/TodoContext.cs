534de4102c13e3d0cedfb6de9069f1caa7962c5c
dx1 <dikeox11@gmail.com>
2026-03-03 03:27:11 -0600

    first commit

---
+++ Models/TodoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MakeHasteApp.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
