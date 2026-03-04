29d60dcbd1f93d27927325f4ceeb870f17c887a0
dx1 <dikeox11@gmail.com>
2026-03-04 01:55:57 -0600

    updated API Logic and added UI

--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -1,12 +1,11 @@
-using System; 
+using System;
 
 namespace MakeHasteApp.Models
 {
-    public class TodoItem
+    public class TodoItemDTO
     {
         public long Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
-        public string? Secret { get; set; }
     }
 }
